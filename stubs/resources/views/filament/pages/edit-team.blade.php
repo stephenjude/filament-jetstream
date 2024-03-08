0308e132a2a9ee77a431fbf2a1eb3b98e946aa1b
Fabrice Planchette <f.planchette@rhinos.fr>
2024-03-08 15:21:32 +0100

    Check gate on delete team

--- stubs/resources/views/filament/pages/edit-team.blade.php
+++ stubs/resources/views/filament/pages/edit-team.blade.php
@@ -3,5 +3,9 @@
 
     @livewire(Laravel\Jetstream\Http\Livewire\TeamMemberManager::class, compact('team'))
 
-    @livewire(Laravel\Jetstream\Http\Livewire\DeleteTeamForm::class, compact('team'))
+    @if (Gate::check('delete', $team) && ! $team->personal_team)
+        <x-section-border/>
+        
+        @livewire(Laravel\Jetstream\Http\Livewire\DeleteTeamForm::class, compact('team'))
+    @endif
 </x-filament-panels::page>
