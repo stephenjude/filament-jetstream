351be58ad2eda8fc8162e3085f24906f18e382b1
stephenjude <31182887+stephenjude@users.noreply.github.com>
2025-08-31 21:38:32 +0000

    Fix styling

--- stubs/app/Models/User.php
+++ stubs/app/Models/User.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Filament\Jetstream\InteractsWIthProfile;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
-use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\LaravelPasskeys\Models\Concerns\HasPasskeys;
+
 // use Filament\Models\Contracts\HasTenants;
 // use Filament\Jetstream\InteractsWithTeams;
 // use Laravel\Sanctum\HasApiTokens;
