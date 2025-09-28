e96642339fb81a59582f145f7b9f39eda6796175
Momin Al Zaraa <64691179+momin-00@users.noreply.github.com>
2025-09-28 12:42:52 +0300

    Refactor team name update section with Filament components

--- resources/views/livewire/teams/update-team-name.blade.php
+++ resources/views/livewire/teams/update-team-name.blade.php
@@ -1,7 +1,14 @@
-<div>
+<x-filament::section aside>
+    <x-slot name="heading">
+        {{ __('filament-jetstream::default.update_team_name.section.title') }}
+    </x-slot>
+    <x-slot name="description">
+        {{ __('filament-jetstream::default.update_team_name.section.description') }}
+    </x-slot>
+
     <form wire:submit="updateTeamName">
         {{ $this->form }}
     </form>
+    <x-filament-actions::modals />
 
-    <x-filament-actions::modals/>
-</div>
+</x-filament::section>
