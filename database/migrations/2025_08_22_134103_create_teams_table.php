1bb1e63e9cf2faa1bbd09ec05cfb73352f84cbd9
stephenjude <31182887+stephenjude@users.noreply.github.com>
2025-10-01 15:12:06 +0000

    Fix styling

--- database/migrations/2025_08_22_134103_create_teams_table.php
+++ database/migrations/2025_08_22_134103_create_teams_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -63,5 +64,4 @@ return new class extends Migration {
 
         Schema::enableForeignKeyConstraints();
     }
-
 };
